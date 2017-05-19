55564686dc45b24cda89e1ba840b3c47109bdf0b
Joe Wass <jwass@crossref.org>
2017-05-19 18:41:02 +0100

    Handle end of input data.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-data-twitter-compliance-patcher "0.1.0"
+(defproject event-data-twitter-compliance-patcher "0.1.1"
   :description "Event Data Twitter Compliance Stream Patcher"
   :url "http://eventdata.crossref.org"
   :license {:name "The MIT License (MIT)"
--- src/event_data_twitter_compliance_patcher/core.clj
+++ src/event_data_twitter_compliance_patcher/core.clj
@@ -57,53 +57,53 @@
 (defn first-compliance-log-chunk
   "Process a compliance log chunk"
   []
-  (let [batch-key (last (store/keys-matching-prefix @storage "twitter/tweet-deletions/"))
-        _ (log/info "Looking at batch" batch-key)
-        ids (json/read-str (store/get-string @storage batch-key))
-        num-ids (count ids)
-        chunks (partition-all 100 ids)
-        num-tweet-ids (atom 0)
-        num-matching-tweet-ids (atom 0)
+  (when-let [batch-key (last (store/keys-matching-prefix @storage "twitter/tweet-deletions/"))]
+    (log/info "Looking at batch" batch-key)
+    (let [ids (json/read-str (store/get-string @storage batch-key))
+          num-ids (count ids)
+          chunks (partition-all 100 ids)
+          num-tweet-ids (atom 0)
+          num-matching-tweet-ids (atom 0)
 
-        ; Filter those tweet IDs that are connected to any Event.
-        ; In chunks.
-        filtered-by-exist-chunks (remove empty?
-                            (pmap (fn [ids]
-                               (let [response (client/get (str (:query-api-base env) "/special/alternative-ids-check") {:as :text :query-params {:ids (clojure.string/join "," ids)}})
-                                     matched-ids (-> response deref :body (json/read-str :key-fn keyword) :alternative-ids)]
-                                (swap! num-tweet-ids #(+ % (count ids)))
-                                (swap! num-matching-tweet-ids #(+ % (count matched-ids)))
-                                (when (zero? (rem @num-tweet-ids 100000))
-                                  (log/info "Matched" @num-matching-tweet-ids "from checked" @num-tweet-ids "from total" num-ids "=" (int (* (/ @num-tweet-ids num-ids) 100)) "%"))
-                                matched-ids)) chunks))
+          ; Filter those tweet IDs that are connected to any Event.
+          ; In chunks.
+          filtered-by-exist-chunks (remove empty?
+                              (pmap (fn [ids]
+                                 (let [response (client/get (str (:query-api-base env) "/special/alternative-ids-check") {:as :text :query-params {:ids (clojure.string/join "," ids)}})
+                                       matched-ids (-> response deref :body (json/read-str :key-fn keyword) :alternative-ids)]
+                                  (swap! num-tweet-ids #(+ % (count ids)))
+                                  (swap! num-matching-tweet-ids #(+ % (count matched-ids)))
+                                  (when (zero? (rem @num-tweet-ids 100000))
+                                    (log/info "Matched" @num-matching-tweet-ids "from checked" @num-tweet-ids "from total" num-ids "=" (int (* (/ @num-tweet-ids num-ids) 100)) "%"))
+                                  matched-ids)) chunks))
 
 
-        ; Find all Event IDs that correspond to Tweet IDs. Some may already have been deleted.
-        ; In chunks.
-        event-ids-chunks (remove empty?
-                           (pmap (fn [ids]
-                                   (mapcat (fn [id]
-                                     (let [response (client/get (str (:query-api-base env) "/events") {:as :text :query-params {:filter (str "source:twitter,alternative-id:" id)}})
-                                            events (-> response deref :body (json/read-str :key-fn keyword) :message :events)]
-                                       (map :id events))) ids))
-                                 filtered-by-exist-chunks))
+          ; Find all Event IDs that correspond to Tweet IDs. Some may already have been deleted.
+          ; In chunks.
+          event-ids-chunks (remove empty?
+                             (pmap (fn [ids]
+                                     (mapcat (fn [id]
+                                       (let [response (client/get (str (:query-api-base env) "/events") {:as :text :query-params {:filter (str "source:twitter,alternative-id:" id)}})
+                                              events (-> response deref :body (json/read-str :key-fn keyword) :message :events)]
+                                         (map :id events))) ids))
+                                   filtered-by-exist-chunks))
 
-        event-ids (apply concat event-ids-chunks)]
+          event-ids (apply concat event-ids-chunks)]
 
-    (log/info "Looking in" batch-key)
-    (doall filtered-by-exist-chunks)
-    (doall event-ids-chunks)
+      (log/info "Looking in" batch-key)
+      (doall filtered-by-exist-chunks)
+      (doall event-ids-chunks)
 
-    (log/info "Interested in tweet IDs" (apply concat filtered-by-exist-chunks))
-    (log/info "Interested in" (count event-ids) "event IDs" event-ids)
+      (log/info "Interested in tweet IDs" (apply concat filtered-by-exist-chunks))
+      (log/info "Interested in" (count event-ids) "event IDs" event-ids)
 
-    (doseq [event-id event-ids]
-      (log/info "Patching event id" event-id)
-      (update-event-id event-id))
+      (doseq [event-id event-ids]
+        (log/info "Patching event id" event-id)
+        (update-event-id event-id))
 
-    ; Only get here if there were no errors.
-    (log/info "Deleting batch key" batch-key)
-    (store/delete @storage batch-key)))
+      ; Only get here if there were no errors.
+      (log/info "Deleting batch key" batch-key)
+      (store/delete @storage batch-key))))
 
 (defn compliance-log-all
   []
